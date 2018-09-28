c9d89f415e84deece11556a38edb9bf5bc4eed37
Ben Williams <8enwilliams@gmail.com>
2018-09-28 14:25:25 +1000

    install generator now found

---
+++ lib/generators/locomotive_subscriptions/install_generator.rb
@@ -0,0 +1,18 @@
+module LocomotiveSubscriptions
+  class InstallGenerator < Rails::Generators::NamedBase
+    source_root File.expand_path('../templates', __FILE__)
+    
+    def require_subscriptions
+      inject_into_file 'lib/locomotivecms.rb', after: "require 'locomotive/engine'\n" do <<-'RUBY'
+
+        require 'locomotive_subscriptions'
+
+      RUBY
+      end
+    end
+
+    def copy_specs
+
+    end
+  end
+end
